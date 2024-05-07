6e83605a4064bfe9c2467114dc00c831a910df92
Jan Kriz <krizj@btlnet.com>
2024-05-07 23:42:39 +0200

    Bootstrap added

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page_view
+#from .views import home_page_view #previous option if home_page_view(request) method was called from viewes.py
+from .views import HomePageView
 
-
-urlpatterns =  [
-    path("", home_page_view, name="home"),
+urlpatterns = [
+   # path("", home_page_view, name="home"),  # see comment
+    path("", HomePageView.as_view(), name="home"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,13 @@
-from django.http import HttpResponse
 
-def home_page_view(request):
-    return HttpResponse("Ahoj")
+'''
+This is the previous, easiest option how to show html response
+'''
+#from django.http import HttpResponse
+
+#def home_page_view(request):
+#   return HttpResponse("Ahoj")
+
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
