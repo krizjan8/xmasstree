62f1b24a8bf07854af3d684405680d21681bdfc0
Jan Kriz <krizj@btlnet.com>
2024-10-12 22:45:54 +0200

    HTML page on local host is working. Frazer + Color picker. Answe in cmd.

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-#from .views import home_page_view #previous option if home_page_view(request) method was called from viewes.py
+from .views import Click_view #previous option if home_page_view(request) method was called from viewes.py
 from .views import HomePageView
+from .views import Color_action
 
 urlpatterns = [
-   # path("", home_page_view, name="home"),  # see comment
+    path("click/", Click_view, name="click"),  # see comment
+    path("colorAction/", Color_action, name="color_action"),
     path("", HomePageView.as_view(), name="home"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,12 +2,21 @@
 '''
 This is the previous, easiest option how to show html response
 '''
-#from django.http import HttpResponse
+from django.http import HttpResponse
 
-#def home_page_view(request):
-#   return HttpResponse("Ahoj")
+def Click_view(request):
+   print('click')
+   return HttpResponse('')
+
+def Color_action(request):
+   print('color')
+   if request.method == 'POST':
+       color = request.POST.get('color')
+       print(color)
+   return HttpResponse('')
 
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
